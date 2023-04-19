d1f88aaf4ac217cbeb09e85d70965f42915e3a22
Vancompernolle, Antoine (ext) <antoine.vancompernolle.ext@suez.com>
2023-04-19 13:50:53 +0000

    Merged PR 1891: feat: #3721 ineligible user
    feat: #3721 ineligible user
    
    Related work items: #3721

---
+++ src/pages/mon-compteur-dechets/index.page.tsx
@@ -0,0 +1,7 @@
+export default function monCompteurDechets() {
+  return (
+    <section className="o-Page__Section">
+      <div>Mon compteur dechets</div>
+    </section>
+  );
+}
---
+++ src/pages/mon-compteur-dechets/non-egibilite/index.page.tsx
@@ -0,0 +1,36 @@
+import "./non-eligibilite-page.scss";
+import CommonButton from "../../../components/Common/CommonButton/CommonButton";
+import { useRouter } from "next/router";
+
+export default function ActualitesPage() {
+  const router = useRouter();
+
+  const firstText =
+    "Malheuresement, le compteur déchets n'est pas encore disponible pour vous";
+  const secondText =
+    " mais vous pouvez accéder à dautres services et conseils pour mieux comprendre et améliorer la gestion de vos déchets !";
+
+  const formLabels = {
+    submitButtonLabel: "J'accède à l'accueil",
+  };
+
+  return (
+    <div className="c-NonEligibilitePage">
+      <div className="c-NonEligibilitePage__Illustration">
+        <div className="c-NonEligibilitePage__Fonctionality">
+          <p className="c-NonEligibilitePage__Title">{firstText}</p>
+          <div className="c-NonEligibilitePage__ImageSvg" />
+          <p className="c-NonEligibilitePage__SecondText">{secondText}</p>
+          <div className="c-NonEligibilitePage__Buttons">
+            <CommonButton
+              type="submit"
+              label={formLabels.submitButtonLabel}
+              style="primary"
+              onClick={() => router.push(`/`)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
