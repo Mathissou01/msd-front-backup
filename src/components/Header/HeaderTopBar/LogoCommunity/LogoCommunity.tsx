b5765a5ce2bfa5bedcb4782e450d5d8ea62f6448
Souleymane Diallo <souleymane.diallo@smile.fr>
2023-08-07 13:17:56 +0000

    Merged PR 3077: fix: #5638 fix responsive logo on mobile
    fix: #5638 fix responsive logo on mobile
    
    Related work items: #5638

--- src/components/Header/HeaderTopBar/LogoCommunity/LogoCommunity.tsx
+++ src/components/Header/HeaderTopBar/LogoCommunity/LogoCommunity.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { makePublicAssetPath } from "../../../../lib/utilities";
 import { useContract } from "../../../../hooks/useContract";
+import "./logo-community.scss";
 
 export default function LogoCommunity() {
   /*Local Data */
